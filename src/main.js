9b47be211ef5efc69f44900c923045465d30d084
Joel Bech <joel.bch@hotmail.se>
2024-05-03 12:46:45 +0200

    will it still work?

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import screenShotimgURL from "../media/screenShot.png";
+import screenShotimgURL from "./media/screenShot.png";
 
 const imgEl = document.createElement('img');
 document.body.append(imgEl)
 imgEl.src = screenShotimgURL;
+
+imgEl.addEventListener("click", event => {
+    console.log("PeePeePooPoo")
+})
 
 console.log("HejHopp")
 
