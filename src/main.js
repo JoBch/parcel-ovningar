abb3f9616f672418a5a287e5b5bdd48cca6e204f
Joel Bech <joel.bch@hotmail.se>
2024-05-03 12:36:15 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import screenShotimgURL from "../media/screenShot.png";
+
+const imgEl = document.createElement('img');
+document.body.append(imgEl)
+imgEl.src = screenShotimgURL;
+
+console.log("HejHopp")
+
